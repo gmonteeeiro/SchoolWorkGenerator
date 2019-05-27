ec50ee225908dc710859b3854d5cc72ee35529e1
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-27 01:40:21 -0300

    Faz a busca pelo termo de pesquisa na Wikipedia

--- robots/text.js
+++ robots/text.js
@@ -1,5 +1,23 @@
+const algorithmia = require('algorithmia');
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
+
 function robot(schoolworkContent){
-    console.log(schoolworkContent);
+    fetchWikipediaContent();
+    // sanitizeContent();
+    // breakContentIntoSentences();
+
+    async function fetchWikipediaContent(){
+        let wikipediaInput = {
+            "articleName": schoolworkContent.info.searchTerm,
+            "lang": "pt"
+          };
+
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
+        const wikipediaAlgorith = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
+        const WikipediaResponse = await wikipediaAlgorith.pipe(wikipediaInput);
+        const WikipediaContent = WikipediaResponse.get();
+        console.log(WikipediaContent)
+    }
 }
 
 module.exports = robot;
